9fb9ad5bf40db7a46a93e5c49457d4d087688d96
user <klisinvladimir13@gmail.com>
2022-11-20 15:23:05 +0700

    TWO project

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QPen, QPixmap, QTransform, QImage, QColor, qRgba, qRed, qGreen, qBlue
 from PyQt5.QtWidgets import QWidget, QApplication, QFileDialog, QMainWindow, QGraphicsItem, QLabel, QInputDialog
 
-SCREEN_SIZE = [500, 500]
+SCREEN_SIZE = [500, 500 ]
 
 
 class MyWidget(QMainWindow):
